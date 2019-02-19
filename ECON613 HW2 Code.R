4e4d835b140cd7872d573380e90bf68a7d115929
YaxuanJiao <46727748+YaxuanJiao@users.noreply.github.com>
2019-02-18 23:36:37 -0500

    Add files via upload

--- ECON613 HW2 Code.R
+++ ECON613 HW2 Code.R
@@ -1,4 +1,5 @@
 #Exercise 1 Data Creation
+
 set.seed(100)
 #construct X1
 X1 <- as.vector(runif(10000,1,3)) 
@@ -12,24 +13,26 @@ eps <- as.vector(rnorm(10000,2,1))
 Y <- 0.5 + 1.2*X1 - 0.9*X2 + 0.1*X3 + eps
 ydum <- ifelse(Y>mean(Y),1,0)
 
+
 #Exercise 2 OLS
-#calculate the correlation between Y and X1
+
+#Question 1: calculate the correlation between Y and X1
 corr_Y_X1 <- cor(Y,X1)
 print(corr_Y_X1)
 
-#regression of Y on X
+#Question 2: regression of Y on X
 a <- data.frame(1,X1,X2,X3)
 X <- data.matrix(a)
 colnames(X) <- c("1","X1","X2","X3")
 y <- data.matrix(Y)
 eps <- data.matrix(eps)
-#calculate the coefficients on the regression
+#Question 3: calculate the coefficients on the regression
 b <- (t(X)%*%X)
 det(b)
 beta = (solve(t(X)%*%X)%*%t(X)%*%y)
 t(beta)
 
-#calculate the standard errors using OLS
+#Question 4 (1): calculate the standard errors using OLS
 #estimate of sigma^2
 sigma2_hat <- (t(eps)%*%eps) / (nrow(X)-ncol(X))
 sigma2_hat
@@ -39,13 +42,14 @@ var_beta_X_hat
 #ols estimate of standard error
 sqrt(diag(var_beta_X_hat))
 
-#calculate the standard errors using bootstrap
+#Question 4 (2): calculate the standard errors using bootstrap
 #49 replications
-boot_49 <- matrix(nrow=10000,ncol=4)
+#create an empty matrix to store sample values
+boot_49 <- matrix(0,nrow=10000,ncol=4)
 colnames(boot_49)<-c("X1","X2","X3","eps")
 aa <- data.frame(X1,X2,X3,eps)
 bb <- data.matrix(aa)
-sdboot_49 <- matrix(nrow = 49,ncol=4)
+sdboot_49 <- matrix(0,nrow = 49,ncol=4)
 for (i in 1:49) {
   for (j in 1:10000) {
     n <- sample(1:nrow(bb), size=1,replace = TRUE)
@@ -54,6 +58,7 @@ for (i in 1:49) {
   }
   xmatrix <- as.matrix(cbind(1,boot_49[,1],boot_49[,2],boot_49[,3]))
   epsmatrix <- as.matrix(boot_49[,4])
+  #calculate stabdard error within the bootstrape
   sigma2_hat1 <- t(epsmatrix)%*%epsmatrix / (nrow(xmatrix)-ncol(xmatrix))
   var_beta_X_hat1 <- c(sigma2_hat1) *  solve(t(xmatrix)%*%xmatrix)
   cal_sd <- sqrt(diag(var_beta_X_hat1))
@@ -61,11 +66,9 @@ for (i in 1:49) {
 }
 
 #499 replication
-boot_499 <- matrix(nrow=10000,ncol=4)
+boot_499 <- matrix(0,nrow=10000,ncol=4)
 colnames(boot_499)<-c("X1","X2","X3","eps")
-aa <- data.frame(X1,X2,X3,eps)
-bb <- data.matrix(aa)
-sdboot_499 <- matrix(nrow = 499,ncol=4)
+sdboot_499 <- matrix(0,nrow = 499,ncol=4)
 for (i in 1:499) {
   for (j in 1:10000) {
     n <- sample(1:nrow(bb), size=1,replace = TRUE)
@@ -80,20 +83,22 @@ for (i in 1:499) {
   sdboot_499[i,] <- cal_sd
 }
 
+
 #Exercise 3
-#Implement the steepest ascent optimization algorithm to maximize that likelihood
-#probit likelihood function
-probit_like <- function(beta,X.=X,ydum.=ydum){
-  beta<-as.matrix(beta)
-  cdf <- pnorm(X.%*%beta)
+
+#Question 1: write a function that returns the likelihood of the probit
+beta1 <- as.matrix(solve(t(X)%*%X)%*%t(X)%*%ydum)
+probit_like <- function(beta1,X.=X,ydum.=ydum){
+  cdf <- pnorm(X.%*%beta1)
   #eliminate extreme values to be capable of plugging into log function
   cdf[cdf>0.99999] <- 0.99999
   cdf[cdf<0.00001]<- 0.00001
   log1 <- sum(ydum.*log(cdf))+sum((1-ydum.)*log(1-cdf))
   return(-log1)
 }
-#build up steepest function
-#define error term
+
+#Question 2: implement the steepest ascent optimization algorithm to maximize likelihood
+#define initial error term
 e <- 0.0001
 #set initial scaling parameter
 alpha = 1
@@ -101,10 +106,10 @@ alpha = 1
 d = 1
 #set initial old parameter
 bo=c(0,0,0,0)
-steepest_asent <- function(beta,probit_like){
+steepest_asent <- function(beta1,probit_like){
   #define tolarance level
   while(d > 0.000001){
-    bo. <- matrix(bo, 4,4)
+    bo. <- matrix(bo,4,4)
     b1 <- bo. + diag(e,4)
     #fbo. <- probit_like(beta=bo.)
     #fb1. <- probit_like(beta=b1.)
@@ -114,15 +119,15 @@ steepest_asent <- function(beta,probit_like){
     #calculate the direction 
     direc <- (fb1-fbo.)/e
     #define optimized scaling parameter with backtracking armijo line-search
-    p1 <- probit_like(beta = bo - alpha*direc)
-    p2 <- probit_like(beta = bo) - 0.1*alpha*t(direc)%*%direc
+    p1 <- probit_like(beta1  = bo - alpha*direc)
+    p2 <- probit_like(beta1  = bo) - 0.1*alpha*t(direc)%*%direc
     if (p1>p2){
       alpha <- 0.5*alpha
       next
     }
     bn <- bo - alpha*direc
-    fbn <- probit_like(beta = bn)
-    fbo <- probit_like(beta = bo)
+    fbn <- probit_like(beta1 = bn)
+    fbo <- probit_like(beta1 = bo)
     #fbn <- apply(bn,2,probit_like)
     #fbo <- apply(bo,2,probit_like)
     d = fbn - fbo
@@ -132,11 +137,15 @@ steepest_asent <- function(beta,probit_like){
 }
 steepest_asent(bo,probit_like)
 
+#Question 3: how different are the paraameters from the true parameters? See in the output document 
+
+
 #Exercise 4 Discrete Choice
+
+#Question 1: write and optimize the probit,logit, and the linear probability model
 #probit likelihood function
-beta <- as.matrix(beta)
-probit_likelihood <- function(beta,X.=X,ydum.=ydum){
-  cdf <- pnorm(X.%*%beta)
+probit_likelihood <- function(beta1,X.=X,ydum.=ydum){
+  cdf <- pnorm(X.%*%beta1)
   #eliminate extreme values to be capable of plugging into log function
   cdf[cdf>0.99999] <- 0.99999
   cdf[cdf<0.00001]<- 0.00001
@@ -144,11 +153,14 @@ probit_likelihood <- function(beta,X.=X,ydum.=ydum){
   return(-log2)
 }
 #probit model optimization
-probit_optim <- optim(par = beta,probit_likelihood)
+probit_optim <- optim(par = beta1,probit_likelihood)
+#construct the probit optimization coefficient 
+probit_optim_beta <- probit_optim$par
+print(probit_optim_beta)
 
 #logit likelihood function
-logit_likelihood <- function(beta,X.=X,ydum.=ydum){
-  cdf <- exp(X.%*%beta)/(1+exp(X.%*%beta))
+logit_likelihood <- function(beta1,X.=X,ydum.=ydum){
+  cdf <- exp(X.%*%beta1)/(1+exp(X.%*%beta1))
   #eliminate extreme values to be capable of plugging into log function
   cdf[cdf>0.99999] <- 0.99999
   cdf[cdf<0.00001]<- 0.00001
@@ -156,22 +168,29 @@ logit_likelihood <- function(beta,X.=X,ydum.=ydum){
   return(log3)
 }
 #logit model optimization
-logit_optim <- optim(par = beta,logit_likelihood)
+logit_optim <- optim(par = beta1,logit_likelihood)
+#construct the logit optimization coefficient 
+logit_optim_beta <- logit_optim$par
+print(logit_optim_beta)
 
 #linear probability model optimization
-linear_model <- function(belta,X.=X,ydum.=ydum){
-  ydum_hat <- X%*%beta
+linear_model <- function(beta1,X.=X,ydum.=ydum){
+  ydum_hat <- X%*%beta1
   SSR <- sum((ydum_hat-ydum)^2)
   return(SSR)
 }
 #linear probability optimization
-linear_optim <- optim(par = beta,linear_model,hessian = TRUE) 
+linear_optim <- optim(par = beta1,linear_model,hessian = TRUE) 
+#construct the linear optimization coefficient 
+linear_optim_beta <- linear_optim$par
+print(linear_optim_beta)
 
+#Question 2: interpret and compare the estimated coefficients(see in the output document) and test on the significance
 #test significance
 dataset <- data.frame(ydum,X1,X2,X3)
 coef_probit <- glm(ydum~X1+X2+X3, family = binomial(link = "probit"),data = dataset)
 summary(coef_probit)
-coef_logit <- glm(ydum~X1+X2+X3, family = binomial(link = "logit"),data = dataset)
+coef_logit <- glm(ydum~X1+X2+X3, family = binomial,data = dataset)
 summary(coef_logit)
 coef_linear <- lm(ydum~X1+X2+X3,data = dataset )
 summary(coef_linear)
@@ -180,52 +199,73 @@ summary(coef_linear)
 install.packages("numDeriv")
 library(numDeriv)
 #establish a function to calculate the marginal effect by probit model
-beta <- as.matrix(beta)
 X <- as.matrix(cbind(1,X1,X2,X3))
-marginal_probit <- function(beta.,X.){
-  pmulti <- X.%*%beta.
+marginal_probit <- function(beta1,X){
+  pmulti <- X%*%beta1
   ppdf <- dnorm(pmulti)
-  me_probit <-ppdf%*%t(beta.)
+  me_probit <-ppdf%*%t(beta1)
   return(me_probit)
 }
-ME_probit <- marginal_probit(probit_optim$par,X)
+ME_probit <- marginal_probit(probit_optim_beta,X)
 
 #establish a function to calculate the marginal effect by logit model
-marginal_logit <- function(beta.,X.){
-  lmulti <- X.%*%beta.
+marginal_logit <- function(beta1,X){
+  lmulti <- X%*%beta
   lpdf <- (exp(lmulti)/((1+exp(lmulti))^2))
-  me_lrobit <-lpdf%*%t(beta.)
-  return(me_lrobit)
+  me_logit <-lpdf%*%t(beta)
+  return(me_logit)
 }
-ME_logit <- marginal_logit(logit_optim$par,X)
+ME_logit <- marginal_logit(logit_optim_beta,X)
 
 write.csv(ME_probit,file = "~/Desktop/Probit Marginal Effect.csv", row.names = FALSE)
 write.csv(ME_logit,file = "~/Desktop/Logit Marginal Effect.csv", row.names = FALSE)
 
-#Compute the standard deviations using bootstrap
-boot_490 <- matrix(nrow=10000,ncol=5)
-margin_490 <- matrix(nrow = 490,ncol=4)
-colnames(boot_490)<-c("constant","X1","X2","X3","ydum")
-dd <- data.frame(1,X1,X2,X3,ydum)
+#Compute the standard deviations of marginal effects using bootstrap
+#probit
+boot_490 <- matrix(0,nrow=10000,ncol=4)
+pmargin_490 <- matrix(0,nrow = 490,ncol=4)
+xmean <- matrix(0,nrow = 490,ncol = 4)
+dd <- data.frame(X1,X2,X3,ydum)
 ee <- data.matrix(dd)
-for (m in 1:490){ 
-  for (n in 1:10000){
+for (i in 1:490){ 
+  for (j in 1:10000){
     f <- sample(1:nrow(ee),size = 1, replace = TRUE)
     g <- matrix(ee[f,])
-    boot_490[n,] <- t(g)
+    boot_490[j,] <- t(g)
   }
-  datax <- as.matrix(cbind(boot_490[,1],boot_490[,2],boot_490[,3],boot_490[,4]))
-  dataydum <- as.matrix(boot_490[,5])
-  mean_x0 <- as.matrix(mean(boot_490[,1]))
-  mean_x1 <- as.matrix(mean(boot_490[,2]))
-  mean_x2 <- as.matrix(mean(boot_490[,3]))
-  mean_x3 <- as.matrix(mean(boot_490[,4]))
-  mean_x <- cbind(mean_x0,mean_x1,mean_x2,mean_x3)
-  cal_coef <- (solve(t(boot_490[,1:4])%*%boot_490[,1:4])%*%t(boot_490[,1:4])%*%boot_490[,5])
-  nie <- mean_x%*%cal_coef
-  cal_margin <- cal_coef%*%nie
-  margin_490[m,] <- cal_margin 
+  mean_x0 <- as.matrix(mean(1))
+  mean_x1 <- as.matrix(mean(boot_490[,1]))
+  mean_x2 <- as.matrix(mean(boot_490[,2]))
+  mean_x3 <- as.matrix(mean(boot_490[,3]))
+  mean_x <- as.matrix(cbind(mean_x0,mean_x1,mean_x2,mean_x3))
+  xmean[i,] <- as.matrix(mean_x)
+  cal_margin <- as.matrix(pnorm(xmean[i,]%*%probit_optim_beta)%*%t(probit_optim_beta))
+  pmargin_490[i,] <- cal_margin
 }
-re = as.function()
-sd(margin_490)
-sdboot_490 <- as.matrix(apply(margin_490, 2,sd))
+sdboot_probit <- as.matrix(apply(pmargin_490, 2,sd))
+
+#logit
+lmargin_490 <- matrix(0,nrow = 490,ncol=4)
+for (i in 1:490){ 
+  for (j in 1:10000){
+    f <- sample(1:nrow(ee),size = 1, replace = TRUE)
+    g <- matrix(ee[f,])
+    boot_490[j,] <- t(g)
+  }
+  mean_x0 <- as.matrix(mean(1))
+  mean_x1 <- as.matrix(mean(boot_490[,1]))
+  mean_x2 <- as.matrix(mean(boot_490[,2]))
+  mean_x3 <- as.matrix(mean(boot_490[,3]))
+  mean_x <- as.matrix(cbind(mean_x0,mean_x1,mean_x2,mean_x3))
+  xmean[i,] <- as.matrix(mean_x)
+  cal_marginn <- (exp(xmean[i,]%*%logit_optim_beta)/((1+exp(xmean[i,]%*%logit_optim_beta))^2))%*%t(logit_optim_beta)
+  lmargin_490[i,] <- cal_marginn
+}
+sdboot_logit <- as.matrix(apply(lmargin_490, 2,sd))
+
+#compute the standard error of marginal effects using the delta method 
+#calculate the variance covariance matrix for probit/logit
+myprobit <- glm(ydum~X1+X2+X3,family = binomial(link = "probit"),data = dataset)
+v_probit <- vcov(myprobit)
+mylogit <- glm(ydum~X1+X2+X3,family = binomial,data = dataset)
+v_logit <- vcov(mylogit)
